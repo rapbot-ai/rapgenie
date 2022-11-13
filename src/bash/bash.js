886b409c71e1695cda6a71c251f66f0003da0fa0
Martin Connor <mepc36@gmail.com>
2022-11-13 10:02:26 -0500

    Run infer command on /POST request.

---
+++ src/bash/bash.js
@@ -0,0 +1,82 @@
+const { spawn } = require('child_process')
+const { appendFileSync } = require('fs')
+
+const execComm = (comm, opts = {}, logFileName) => {
+  const { needsData, printLogs, saveLogs } = opts
+
+  return new Promise((resolve, reject) => {
+    const terminal = spawn('bash')
+
+    terminal.stdout.on('data', (data) => {
+      if (needsData) {
+        resolve(data.toString())
+      }
+      printLogs && console.log('### data:', data)
+      saveLogs && appendFileSync(`./output/bash/${logFileName}.txt`, JSON.stringify(data))
+    });
+
+    terminal.on('exit', async (code) => {
+      if (code) {
+        reject(new Error(`Failed running comm:\n${comm}`))
+      } else {
+        resolve()
+      }
+    });
+
+    // FFMPEG LOGS ITS PRINTOUTS TO .STDERR, SO WE COMMENT IT OUT: 
+    // eslint-disable-next-line no-unused-vars
+    terminal.stderr.on('data', (data) => {
+      printLogs && console.log(`###########\n${data}`);
+      saveLogs && appendFileSync(`./output/bash/${logFileName}.txt`, JSON.stringify(data.toString()))
+    });
+
+    terminal.stdin.write(comm, async (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        terminal.stdin.end();
+      }
+    });
+  })
+}
+
+const execPythonComm = (args, opts = {}) => {
+  const { printLogs } = opts
+  return new Promise((resolve, reject) => {
+    const terminal = spawn('/bin/usr/python3', args)
+
+    // eslint-disable-next-line no-unused-vars
+    // ALWAYS MUST BE LEFT ON, OR ELSE PROCESS WON'T EXECUTE FOR SOME REASON:
+    terminal.stdout.on('data', (data) => {
+      printLogs && console.log('$$$ data:', data.toString())
+      return resolve(data.toString())
+    });
+
+    terminal.on('exit', async (code) => {
+      if (code) {
+        return reject(new Error(`Failed running comm:\n${args}`))
+      } else {
+        return resolve()
+      }
+    });
+
+    terminal.stderr.on('data', (error) => {
+      console.log(`STDERR > error: ${error}`);
+      console.log(`STDERR > error.toString(): ${error && error.toString()}`);
+      // return reject(error)
+    });
+
+    terminal.stdin.write('', async (err) => {
+      if (err) {
+        return reject(new Error(`Error creating file 2\ncode: ${code} signal:${signal}`))
+      } else {
+        terminal.stdin.end();
+      }
+    });
+  })
+}
+
+module.exports = {
+  execComm,
+  execPythonComm
+}
--- src/express/express.js
+++ src/express/express.js
@@ -2,14 +2,38 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
 const CORS = require('cors');
+const { execPythonComm } = require('../bash/bash')
 
 const app = express()
 
 app.use(CORS());
 app.use(bodyParser.json({ strict: false, limit: '50mb' }));
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
+app.get('/', async (req, res) => {
+  return res.send('Hello world!')
+})
+
+app.post(`/infer`, async (req, res) => {
+  try {
+    const inferPath = `/home/ubuntu/1-radtts-repo/inference.py`
+    const modelConfigPath = `-c /home/ubuntu/1-radtts-repo/2-configs/1-radtts-configs/config_ljs_dap.json`
+    const modelPath = `-r /home/ubuntu/1-radtts-repo/1-models/1-radtts-models/lupe-fiasco-radtts-model`
+    const vocoderPath = `-v /home/ubuntu/1-radtts-repo/1-models/2-hifigan-models/hifigan_libritts100360_generator0p5.pt`
+    const vocoderConfigPath = `-k /home/ubuntu/1-radtts-repo/2-configs/2-hifigan-configs/hifigan_22khz_config.json`
+    const textInput = `-t /home/ubuntu/1-radtts-repo/5-tts-input-text/catch-me-like-spalding.txt`
+    const speaker = `-s lupefiasco`
+    const speakerAttributes = `--speaker_attributes lupefiasco`
+    const speakerText = `--speaker_text lupefiasco`
+    const outputDir = `-o /home/ubuntu/1-radtts-repo/6-training-output`
+    const radttsInferCommand = `${inferPath} ${modelConfigPath} ${modelPath} ${vocoderPath} ${vocoderConfigPath} ${textInput} ${speaker} ${speakerAttributes} ${speakerText} ${outputDir}`.split(' ')
+    const result = await execPythonComm(radttsInferCommand, { printLogs: true })
+    console.log('radttsInferCommand:', [`python3`, inferPath, modelConfigPath, modelPath, vocoderPath, vocoderConfigPath, textInput, speaker, speakerAttributes, speakerText, outputDir].join(` \\\n`))
+    return res.send(result)
+  } catch (error) {
+    console.log('error:', error)
+    const stringifiedError = JSON.stringify(error, Object.getOwnPropertyNames(error))
+    res.status(500).send(stringifiedError)
+  }
 })
 
 app.listen(3020, async () => {
