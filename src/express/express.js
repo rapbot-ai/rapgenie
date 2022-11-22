9e9af7c1b913a4cd4aca5d1616506c726a8fffe7
Martin Connor <mepc36@gmail.com>
2022-11-22 09:50:29 -0500

    Voice transfer using typecast API.

--- src/express/express.js
+++ src/express/express.js
@@ -3,9 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const CORS = require('cors');
 const { v4 } = require('uuid')
-const { writeFileSync, rmSync, mkdirSync, existsSync, createReadStream } = require('fs')
-const { uploadToS3, s3Client } = require('../aws/aws')
-const { execPythonComm } = require('../bash/bash')
+const { writeFileSync, rmSync, mkdirSync, existsSync, createReadStream, createWriteStream } = require('fs')
+const { uploadToS3, s3Client } = require('../aws/aws.js')
+const { execPythonComm } = require('../bash/bash.js')
 
 const app = express()
 
@@ -65,9 +65,9 @@ app.post(`/infer`, async (req, res) => {
 
     await execPythonComm(radttsInferCommand, { printLogs: true })
 
-    const radttsOutputWav = `0_0_lupefiasco_durscaling1.0_sigma0.8_sigmatext0.666_sigmaf01.0_sigmaenergy1.0_denoised_0.0.wav`
-    console.log('wavPath:', `${radttsOutputDir}/${radttsOutputWav}`)
-    const wavContent = createReadStream(`${radttsOutputDir}/${radttsOutputWav}`)
+    const inferOutput = `0_0_lupefiasco_durscaling1.0_sigma0.8_sigmatext0.666_sigmaf01.0_sigmaenergy1.0_denoised_0.0.wav`
+    console.log('wavPath:', `${radttsOutputDir}/${inferOutput}`)
+    const wavContent = createReadStream(`${radttsOutputDir}/${inferOutput}`)
     await uploadToS3(`${jobId}.wav`, 'rapbot-rapgenie-outputs', wavContent, 'audio/wav')
     const params = {
       Bucket: 'rapbot-rapgenie-outputs',
@@ -84,6 +84,134 @@ app.post(`/infer`, async (req, res) => {
     res.status(500).send(stringifiedError)
   }
 })
+
+app.post(`/infer-typecast`, async (req, res) => {
+  try {
+    const { inferenceBody, inferenceType } = req.body
+    const jobId = v4()
+    console.log('jobId:', jobId)
+    const gptLyricsFile = `/home/ubuntu/1-radtts-repo/5-tts-input-text/${jobId}.txt`
+
+    if (inferenceType === 'text' && inferenceBody) {
+      console.log("Text-based STT...")
+    } else if (inferenceType === 'topic' && inferenceBody) {
+      console.log("Topic-based STT...")
+      const getCoupletsFunc = `/home/ubuntu/1-radtts-repo/src/gpt/get-couplet.py`
+      const getCoupletsCommand = [getCoupletsFunc, inferenceBody, gptLyricsFile,]
+      await execPythonComm(getCoupletsCommand, { printLogs: true })
+    } else if (inferenceType !== 'topic' || !inferenceType === 'text' || '') {
+      throw new Error(`inferenceType must be 'topic' or 'text'`)
+    } else if (!inferenceBody) {
+      throw new Error(`'inferenceBody' must be defined!`)
+    }
+
+    const radttsOutputDir = `/home/ubuntu/1-radtts-repo/6-training-output/${jobId}`
+
+    inferenceType === 'text' && writeFileSync(gptLyricsFile, inferenceBody)
+    !existsSync(radttsOutputDir) && mkdirSync(radttsOutputDir)
+    !existsSync(`${radttsOutputDir}/wavs`) && mkdirSync(`${radttsOutputDir}/wavs`)
+
+    const dollarJrActorId = '61b007392f2010f2aa1a052a'
+    const tempo = 0.5
+    const style_label = 'tonedown-1'
+    const lang = 'en'
+    const max_seconds = 20
+    const text = inferenceType === 'text' ?
+      inferenceBody :
+      readFileSync(gptLyricsFile, 'utf-8')
+
+    const body = {
+      text,
+      lang,
+      actor_id: dollarJrActorId,
+      max_seconds,
+      tempo,
+      style_label
+    }
+    const { data: { result: { speak_url } } } = await axios.post(`https://typecast.ai/api/speak`, body)
+
+    const pollForTypecastJob = async (speak_url) => {
+      const { data: { result: { status, audio } } } = await axios.get(speak_url)
+
+      if (status === 'done') {
+        const { url } = audio
+        return `${url}/no-redirect`
+      } else if (status !== 'done') {
+        return new Promise((resolve, reject) => {
+          return setTimeout(async () => {
+            try {
+              return resolve(await pollForTypecastJob(speak_url))
+            } catch (error) {
+              return reject(error)
+            }
+          }, 1000)
+        })
+      } else {
+        // TODO: handle whatever value for 'status' that typecast uses to indicate errors
+      }
+    }
+
+    const audioFileUrl = await pollForTypecastJob(speak_url)
+
+    const typecastOutput = `${radttsOutputDir}/wavs/1.wav`
+    const writer = createWriteStream(typecastOutput);
+    const streamResponse = await axios.get(audioFileUrl, { responseType: 'stream' });
+    streamResponse.data.pipe(writer);
+
+    await new Promise((resolve, reject) => {
+      writer.on('finish', () => {
+        console.log("Finished")
+        return resolve()
+      });
+      writer.on('error', (error) => {
+        console.error("Error while downloading wav:", error)
+        return reject(error)
+      });
+    })
+
+    const validationString = `1.wav|${text.replace(`\n`, ' ')}|lupefiasco`
+    writeFileSync(`${radttsOutputDir}/validation.txt`, validationString)
+
+    const conversionFunc = `/home/ubuntu/1-radtts-repo/inference_voice_conversion.py`
+    const radttsModelConfig = `-c /home/ubuntu/1-radtts-repo/2-configs/1-radtts-configs/config_ljs_dap.json`
+    const radttsModel = `-r /home/ubuntu/1-radtts-repo/1-models/1-radtts-models/lupe-fiasco-radtts-model`
+    const vocoder = `-v /home/ubuntu/1-radtts-repo/1-models/2-hifigan-models/hifigan_libritts100360_generator0p5.pt`
+    const vocoderConfig = `-k /home/ubuntu/1-radtts-repo/2-configs/2-hifigan-configs/hifigan_22khz_config.json`
+    const validationParams = `"{'Dummy': {'basedir': '${radttsOutputDir}', 'audiodir':'wavs', 'filelist': 'validation.txt'}}"`
+    const validationParamsArg = `-p data_config.validation_files=${validationParams}`
+    const radttsOutputDirArg = `-o ${radttsOutputDir}`
+    const radttsVoiceTransferCommand = [
+      conversionFunc,
+      radttsModel,
+      radttsModelConfig,
+      vocoder,
+      vocoderConfig,
+      radttsOutputDirArg,
+      validationParamsArg
+    ]
+    console.log('radttsVoiceTransferCommand:', radttsVoiceTransferCommand)
+    await execPythonComm(radttsVoiceTransferCommand, { printLogs: true })
+
+    const voiceTransferOutput = `1_0_sid0_sigma0.8.wav`
+    const wavContent = createReadStream(`${radttsOutputDir}/${voiceTransferOutput}`)
+    await uploadToS3(`${jobId}.wav`, 'rapbot-rapgenie-outputs', wavContent, 'audio/wav')
+    const params = {
+      Bucket: 'rapbot-rapgenie-outputs',
+      Key: `${jobId}.wav`
+    }
+    const wavSignedUrl = s3Client.getSignedUrl('getObject', params)
+
+    rmSync(gptLyricsFile)
+    rmSync(radttsOutputDir, { recursive: true, force: true });
+
+    return res.send({ wavSignedUrl })
+  } catch (error) {
+    console.log('error:', error)
+    const stringifiedError = JSON.stringify(error, Object.getOwnPropertyNames(error))
+    res.status(500).send(stringifiedError)
+  }
+})
+
 
 app.post(`/typecast-callback`, async (req, res) => {
   try {
