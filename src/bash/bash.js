b5aa45cd6af53ceec0cbd4398afc686d0a5ecd98
Martin Connor <mepc36@gmail.com>
2022-11-22 12:00:39 -0500

    Rm debug logs.

--- src/bash/bash.js
+++ src/bash/bash.js
@@ -49,38 +49,25 @@ const execPythonComm = (args, opts = {}) => {
     // ALWAYS MUST BE LEFT ON, OR ELSE PROCESS WON'T EXECUTE FOR SOME REASON:
     terminal.stdout.on('data', (data) => {
       printLogs && console.log('$$$ data:', data.toString())
-      console.log('resolve 1')
-      // return resolve(data.toString())
     });
 
     terminal.on('exit', async (code) => {
       if (code) {
         return reject(new Error(`Failed running comm:\n${args}`))
       } else {
-        console.log('resolve 4')
         return resolve()
       }
     });
 
     terminal.stderr.on('data', (error) => {
       console.log('STDERR > error:', error.toString())
-
-      // TODO: actually rethrow errors, i.e.:
-      // const isWarning = error.toString().includes('UserWarning')
-      // console.log('isWarning:', isWarning)
-      // if (!isWarning) {
-      // console.log('Rethrowing...')
-      // const rethrownError = new Error(error.toString())
-      // return reject(rethrownError)
-      // }
+      // TODO: actually throw errs, while filtering out UserWarning's that RADTTS sends
     });
 
     terminal.stdin.write('', async (err) => {
       if (err) {
-        console.log('resolve 2')
         return reject(new Error(`Error creating file 2\ncode: ${code} signal:${signal}`))
       } else {
-        console.log('resolve 3')
         terminal.stdin.end();
       }
     });
